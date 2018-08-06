b48fe302297df0cc374a11bddf34d5fa98dbbb85
Chongsawad Saiaram <nice@eatigo.com>
2018-08-06 13:43:49 +0700

    Initial Commit

---
+++ FirebaseAuthLogin/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  FirebaseAuthLogin
+//
+//  Created by Nice on 6/8/18.
+//  Copyright © 2018 eatigo. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
